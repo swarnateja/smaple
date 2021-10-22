25730b4df48abc0d325c77ce4826043e37271514
swarnateja <achantaswarnateja@gmail.com>
2021-10-22 09:13:52 +0530

    userprofile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
+import UserProfile from './components/UserProfile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const initialUserDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer',
+  },
+]
+
+class App extends Component {
+  state = {
+    searchInput: '',
+    userDetailsList: initialUserDetailsList,
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onDeleteUser = uniqueNo => {
+    console.log('Deleted')
+    const {userDetailsList} = this.state
+    const filteredUserData = userDetailsList.filter(
+      eachUser => eachUser.uniqueNo !== uniqueNo,
+    )
+    this.setState({userDetailsList: filteredUserData})
+  }
+
+  render() {
+    const {searchInput, userDetailsList} = this.state
+    const searchInputValue = searchInput
+    console.log(searchInputValue)
+    const searchResult = userDetailsList.filter(eachUser =>
+      eachUser.name.includes(searchInputValue),
+    )
+    return (
+      <div className="app-container">
+        <h1 className="title">Users List</h1>
+        <input type="search" onChange={this.onChangeSearchInput} />
+        <ul className="list-container">
+          {searchResult.map(eachUser => (
+            <UserProfile
+              onDeleteUser={this.onDeleteUser}
+              userDetails={eachUser}
+              key={eachUser.uniqueNo}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
